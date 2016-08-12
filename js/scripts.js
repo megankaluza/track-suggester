987da14c9ebcb923f0a8c1754f705ee4a0b50286
megxkae xo <megxkae xo>
2016-08-12 14:57:09 -0700

    More descriptive id and class names in HTML, CSS, and JS to call functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,57 @@
 $(document).ready(function(){
   var result = result;
   var click = click;
-  var d3 = $("input#d3").val();
-  var d4 = $("input#d4").val();
-  var d5 = $("input#d5").val();
-  var d6 = $("input#d6").val();
-  var d7 = $("input#d7").val();
-  var d8 = $("input#d8").val();
-  var d9 = $("input#d9").val();
-  var d10 = $("input#d10").val();
-  var d11 = $("input#d11").val();
-  var d12 = $("input#d12").val();
-  var d13 = $("input#d13").val();
-  var d14 = $("input#d14").val();
-  var form2 = $(".form2").val();
-  var form3 = $(".form3").val();
-  var form4 = $(".form4").val();
-  var form5 = $(".form5").val();
-  var form6 = $(".form6").val();
-  var form7 = $(".form7").val();
+
 
   $("button").click(function(){
-    $(".form").fadeIn();
+    $(".intro-form").fadeIn();
   });
-//Developer//
-  $("input#d2").click(function(){
-    $(".form3").show();
+  //Developer//
+  $("input#developer").click(function(){
+    $(".developer-form").show();
   });
-//Designer//
-  $("input#d3").click(function(){
-    $(".form2").show();
+  //Designer//
+  $("input#designer").click(function(){
+    $(".designer-form").show();
   });
-//Designer: Choose Manipulate your own design//
-  $("input#d4").click(function(){
-    $(".form6").show();
+  //Designer: Choose Manipulate your own design//
+  $("input#illustrate").click(function(){
+    $(".user-form").show();
   });
-//Designer: Choose Manipulate Templates//
- $("input#d5").click(function(){
-   $(".form4").show();
- });
-
- //Designer: Choose develop Wordpress, Drupal, and Joomla//
- //IF STATEMENT(PHP AND DRUPAL)//
-//Designer: Choose develop Android//
-//IF STATEMENT(Java AND Android)//
-
-
-//Designer: Choose User Interface (UI) & atomic design//
-//IF STATEMENT(CSS AND DESIGN)
-//Designer: Choose Object-oriented design basics//
-//IF STATEMENT(C# AND .NET
-
-//Developer: Choose start-up company//
-$("input#d6").click(function(){
-  $(".form4").show();
+  //Designer: Choose Manipulate Templates//
+  $("input#manipulate").click(function(){
+    $(".develop-form").show();
   });
 
-//Developer: Choose larger complany//
-$("input#d7").click(function(){
-  $(".form5").show();
-});
-
-//Developer: Choose develop Wordpress, Drupal, and Joomla//
-//IF STATEMENT(PHP AND DRUPAL)//
-//Developer: Choose develop Android//
-//IF STATEMENT(Java AND Android)//
-
-//Developer: Choose Microsoft//
-//IF STATEMENT(JAVA and C#)//
-//Developer: Choose .NET//
-//IF STATEMENT (Ruby and Rails)//
+  //Designer: Choose develop Wordpress, Drupal, and Joomla//
+  //IF STATEMENT(PHP AND DRUPAL)//
+  //Designer: Choose develop Android//
+  //IF STATEMENT(Java AND Android)//
 
 
+  //Designer: Choose User Interface (UI) & atomic design//
+  //IF STATEMENT(CSS AND DESIGN)
+  //Designer: Choose Object-oriented design basics//
+  //IF STATEMENT(C# AND .NET
 
+  //Developer: Choose start-up company//
+  $("input#start-up").click(function(){
+    $(".develop-form").show();
+  });
+
+  //Developer: Choose larger complany//
+  $("input#large").click(function(){
+    $(".micro-form").show();
+  });
+
+  //Developer: Choose develop Wordpress, Drupal, and Joomla//
+  //IF STATEMENT(PHP AND DRUPAL)//
+  //Developer: Choose develop Android//
+  //IF STATEMENT(Java AND Android)//
+
+  //Developer: Choose Microsoft//
+  //IF STATEMENT(JAVA and C#)//
+  //Developer: Choose .NET//
+  //IF STATEMENT (Ruby and Rails)//
 
 });
