932b9c958b3060c64d9c11f0e1ebaa0c203ef904
megxkae xo <megxkae xo>
2016-08-12 11:59:43 -0700

    Adds more survey questions to my HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,41 @@
 $(document).ready(function(){
-    var result = result;
-    var d3 = $("input#d3").val();
-    var d4 = $("input#d4").val();
-    var d5 = $("input#d5").val();
-    var d6 = $("input#d6").val();
-    var d7 = $("input#d7").val();
-    var d8 = $("input#d8").val();
-    var d9 = $("input#d9").val();
-    var d10 = $("input#d10").val();
-    var d11 = $("input#d11").val();
-    var d12 = $("input#d12").val();
-    var d13 = $("input#d13").val();
-    var d14 = $("input#d14").val();
-    var form2 = $(".form2").val();
-    var form3 = $(".form3").val();
-    var form4 = $(".form4").val();
-    var form5 = $(".form5").val();
-    var form6 = $(".form6").val();
-    var form7 = $(".form7").val();
+  var result = result;
+  var d3 = $("input#d3").val();
+  var d4 = $("input#d4").val();
+  var d5 = $("input#d5").val();
+  var d6 = $("input#d6").val();
+  var d7 = $("input#d7").val();
+  var d8 = $("input#d8").val();
+  var d9 = $("input#d9").val();
+  var d10 = $("input#d10").val();
+  var d11 = $("input#d11").val();
+  var d12 = $("input#d12").val();
+  var d13 = $("input#d13").val();
+  var d14 = $("input#d14").val();
+  var form2 = $(".form2").val();
+  var form3 = $(".form3").val();
+  var form4 = $(".form4").val();
+  var form5 = $(".form5").val();
+  var form6 = $(".form6").val();
+  var form7 = $(".form7").val();
 
   $("button").click(function(){
     $(".form").fadeIn();
-      });
-
-    $("input#d2").click(function(){
-      $(".form3").show();
-    });
-    
-    $("input#d3").click(function(){
-      $(".form2").show();
-
-
   });
+
+  $("input#d2").click(function(){
+    $(".form3").show();
+  });
+
+  $("input#d3").click(function(){
+    $(".form2").show();
+  });
+
+  $("input#d5").click(function(){
+    $(".form4").show();
+  });
+
+  $("input#d6").click(function(){
+    $(".form5").show();
+  })
 });
