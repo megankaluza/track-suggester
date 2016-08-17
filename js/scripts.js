c081689b2021f42691c930e4ee2aa0d9df7c8f4f
megxkae xo <megxkae xo>
2016-08-16 22:24:38 -0700

    Show a functioning form with updates to HTML, CSS, and JS withif and if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,64 @@
 $(document).ready(function(){
-  var nameInput = $("#inputName").val();
-  var developerInput = $("input#developer").val();
-  var designerInput = $("input#designer").val();
-  var illustrateInput = $("input#illustrate").val();
-  var manipulateInput = $("input#manipulate").val ();
-  var startInput = $("input#start").val();
-  var largeInput = $("input#large").val();
-  var phpInput = $("input#php").val();
-  var androidInput = $("input#android").val();
-  var microsoftInput = $("input#microsoft").val();
-  var netInput = $("input#net").val();
-  var userInput = $("input#user").val();
-  var objectInput = $("input#object").val();
+  $("form#track").submit(function(event){
+  var questionOneInput = $("input:radio[name=questionOne]:checked").val();
+  var questionTwoInput = $("input:radio[name=questionTwo]:checked").val();
+  var questionThreeInput = $("input:radio[name=questionThree]:checked").val();
+  var questionFourInput = $("input:radio[name=questionFour]:checked").val();
+  var questionFiveInput = $("input:radio[name=questionFive]:checked").val();
+  var questionSixInput = $("input:radio[name=questionSix]:checked").val();
+  var design = 0;
+  var java = 0;
+  var php = 0;
 
-  $("button").click(function(){
-    $(".intro-form").addClass("show");
+if(questionOneInput === "Developer") {
+  php+=1;
+  java+=2;
+} else if (questionOneInput === "Designer") {
+  design+=2;
+};
+
+if (questionTwoInput === "Manipulate Templates") {
+  php+=1;
+} else if (questionTwoInput === "Manipulate your own Design") {
+  design+=2;
+  java+=1;
+};
+
+if (questionThreeInput === "Start-Up Company") {
+  php+=1;
+} else if (questionThreeInput === "Larger Company") {
+  java+=1;
+};
+
+if (questionFourInput === "Develop Wordpress, Drupal, and Joomla") {
+  php+=1;
+  design+=1;
+} else if (questionFourInput === "Develop Android") {
+  java+=1;
+};
+
+if (questionFiveInput === "Back-End Development") {
+  php+=1;
+  java+=1;
+} else if (questionFiveInput === "Front-End Design") {
+  design+=2;
+  java+=1;
+};
+
+if (questionSixInput === "Mobile User Interface (UI)") {
+  java+=2;
+} else if (questionSixInput === "User Interface (UI)") {
+  design+=2;
+};
+
+if (design > php && design > java) {
+   $("#design").addClass("show");
+} else if (php > design && php > java) {
+   $("#php").addClass("show");
+} else if (java > design && java > php) {
+   $("#java").addClass("show");
+};
+
+  event.preventDefault();
   });
-  //Developer//
-  $("input#developer").click(function(){
-    $(".developer-form").addClass("show");
-  });
-  //Designer//
-  $("input#designer").click(function(){
-    $(".designer-form").addClass("show");
-  });
-  //Designer: Choose Manipulate your own design//
-  $("input#illustrate").click(function(){
-    $(".user-form").addClass("show");
-  });
-  //Designer: Choose Manipulate Templates//
-  $("input#manipulate").click(function(){
-    $(".develop-form").addClass("show");
-  });
-  //Designer: Choose develop Wordpress, Drupal, and Joomla//
-  //IF STATEMENT(PHP AND DRUPAL)//
-  $("input#php").click(function(){
-    $("#output").addClass("show");
-  });
-  //Designer: Choose develop Android//
-  //IF STATEMENT(Java AND Android)//
-  $("input#android").click(function(){
-    $("#output2").addClass("show");
-  });
-  //Designer: Choose User Interface (UI) & atomic design//
-  //IF STATEMENT(CSS AND DESIGN)
-  $("input#user").click(function(){
-    $("#output3").addClass("show");
-  });
-  //Designer: Choose Object-oriented design basics//
-  //IF STATEMENT(C# AND .NET)
-  $("input#object").click (function(){
-    $("#output4").addClass("show");
-  });
-  //Developer: Choose start-up company//
-  $("input#start").click(function(){
-    $(".develop-form").addClass("show");
-  });
-  //Developer: Choose larger complany//
-  $("input#large").click(function(){
-    $(".micro-form").addClass("show");
-  });
-  //Developer: Choose Microsoft//
-  //IF STATEMENT(JAVA and C#)//
-  $("input#microsoft").click(function(){
-    $("#output5").addClass("show");
-  });
-  //Developer: Choose .NET//
-  //IF STATEMENT (Ruby and Rails)//
-  $("input#net").click (function(){
-    $("#output6").addClass("show");
-  });
-  //event.preventDefault();
 });
